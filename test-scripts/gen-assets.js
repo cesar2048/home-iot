e53b5aebb354bd062aaa168fdc82a949c0db2fcd
Cesar <cesar.reyes10@gmail.com>
2023-08-04 00:12:23 -0600

    improve html page style and add influx fields

--- test-scripts/gen-assets.js
+++ test-scripts/gen-assets.js
@@ -2,20 +2,32 @@ const zlib = require('node:zlib');
 const { createReadStream, createWriteStream } = require('node:fs');
 const { readFile, writeFile } = require('node:fs/promises');
 const { pipeline } = require('node:stream/promises');
+const minify = require('html-minifier').minify;
 
 const fn = async (sourceFiles, destPath) => {
     let allContent = '';
 
     for (let f =0; f < sourceFiles.length; f++) {
-        const interPath = 'public/intermediate.bin';
-        const sourcePath = sourceFiles[f].path;
+        const interPath1 = 'public/intermediate1.bin';
+        const interPath2 = 'public/intermediate2.bin';
+        let sourcePath = sourceFiles[f].path;
         const symbolName = sourceFiles[f].symbol;
 
+        // preprocess html
+        let content = await readFile(sourcePath, 'utf-8');
+        content = minify(content, {
+            removeAttributeQuotes: true,
+            collapseWhitespace: true,
+            minifyCSS: true
+        });
+        await writeFile(interPath1, content);
+        sourcePath = interPath1;
+
         const source = createReadStream(sourcePath);
-        const dest = createWriteStream(interPath);
+        const dest = createWriteStream(interPath2);
         await pipeline(source, zlib.createGzip(), dest);
     
-        const content = await readFile(interPath);
+        content = await readFile(interPath2);
         const hexText = content.toString('hex').toUpperCase();
         
         const nibbles = [];
